7338fdb5f41a8892d60ff37acce70c00c88fac36
lanar <lanar@DESKTOP-FU3G8AR.fritz.box>
2019-10-08 17:13:03 +0200

    Abstand hinzugefügt

--- ConnectFour/src/connectfour/ui/Spielfeld.java
+++ ConnectFour/src/connectfour/ui/Spielfeld.java
@@ -11,7 +11,7 @@ public class Spielfeld extends JPanel
 	{
 		final int rows = 6;
 		final int cols = 7;
-		this.setLayout(new GridLayout(rows, cols));
+		this.setLayout(new GridLayout(rows, cols, 1, 1));
 		this.setBackground(Color.CYAN);
 		for (int i = 0; i < rows * cols; i++)
 		{
