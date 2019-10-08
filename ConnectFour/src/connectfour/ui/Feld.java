4c623d9dec5145e1f0de454596bfd6a255341dd6
lanar <lanar@DESKTOP-FU3G8AR.fritz.box>
2019-10-08 17:11:06 +0200

    Rand hinzugefügt

--- ConnectFour/src/connectfour/ui/Feld.java
+++ ConnectFour/src/connectfour/ui/Feld.java
@@ -2,6 +2,7 @@ package connectfour.ui;
 
 import java.awt.Color;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
 public class Feld extends JButton
@@ -9,6 +10,8 @@ public class Feld extends JButton
 	public Feld()
 	{
 		this.setBackground(Color.WHITE);
+		this.setBorder(BorderFactory.createLineBorder(Color.RED, 2));
+
 	}
 
 }
