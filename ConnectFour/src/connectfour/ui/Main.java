db050c8744d313caf2d578b3f5e149c4e9469254
lanar <lanar@DESKTOP-FU3G8AR.fritz.box>
2019-10-08 12:49:39 +0200

    initial commit

---
+++ ConnectFour/src/connectfour/ui/Main.java
@@ -0,0 +1,9 @@
+package connectfour.ui;
+
+public class Main {
+	
+		public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hallo Rico, wie geht's?");	}
+
+}
