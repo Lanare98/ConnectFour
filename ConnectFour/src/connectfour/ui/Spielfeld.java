dc28d24af75fb004a74b83cd1ff29bd5801c7df9
lanar <lanar@DESKTOP-FU3G8AR.fritz.box>
2019-10-08 16:54:08 +0200

    Spielfeldgröße festgelegt

--- ConnectFour/src/connectfour/ui/Spielfeld.java
+++ ConnectFour/src/connectfour/ui/Spielfeld.java
@@ -9,8 +9,8 @@ public class Spielfeld extends JPanel
 {
 	public Spielfeld()
 	{
-		final int rows = 5;
-		final int cols = 5;
+		final int rows = 6;
+		final int cols = 7;
 		this.setLayout(new GridLayout(rows, cols));
 		this.setBackground(Color.CYAN);
 	}
